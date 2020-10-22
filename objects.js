4f5a83407e3d3db1965588f50371e45e442db5ca
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-22 08:25:12 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,9 +3,9 @@ var playlist = {artistName: "name", songTitle: "name"}
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist.artistName = songTitle
   return playlist
-  
+
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  
+
 }
