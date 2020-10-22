36f76c55a92ebd93bb7e9c9fe1aa999d4b70298e
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-22 08:44:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,12 +1,11 @@
 var playlist = {artistName: "Julia", songTitle: "Why Come Back"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {artistName: songTitle})
-
+  playlist[artistName] = songTitle
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName
+  delete playlist[artistName]
   return playlist
-
 }
